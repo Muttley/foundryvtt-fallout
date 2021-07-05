1244e8c85d415366be7af93a4938dcc15e5e9044
superseva <superseva@gmail.com>
2021-07-05 23:56:34 +0200

    added new if handler

--- module/helpers/handlebars.mjs
+++ module/helpers/handlebars.mjs
@@ -53,7 +53,7 @@ export const registerHandlebarsHelpers = function () {
         }
     });
 
-    Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
+    Handlebars.registerHelper("math", function (lvalue, operator, rvalue, options) {
         lvalue = parseFloat(lvalue);
         rvalue = parseFloat(rvalue);
         return {
@@ -96,5 +96,20 @@ export const registerHandlebarsHelpers = function () {
             return true;
         else
             return false;
+    });
+
+    Handlebars.registerHelper({
+        eq: (v1, v2) => v1 === v2,
+        ne: (v1, v2) => v1 !== v2,
+        lt: (v1, v2) => v1 < v2,
+        gt: (v1, v2) => v1 > v2,
+        lte: (v1, v2) => v1 <= v2,
+        gte: (v1, v2) => v1 >= v2,
+        and() {
+            return Array.prototype.every.call(arguments, Boolean);
+        },
+        or() {
+            return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+        }
     });
 }
