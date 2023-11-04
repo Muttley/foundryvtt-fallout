c5b22c20be8eed351c8963f4214654344e8c7041
Paul Maskelyne <mu77ley@gmail.com>
2023-11-04 21:53:17 +0000

    i18n files now created at build time

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,36 +1,35 @@
 import gulp from "gulp";
 
 import * as css from "./utils/css.mjs";
-// import * as lang from "./utils/lang.mjs";
+import * as lang from "./utils/lang.mjs";
 import * as javascript from "./utils/javascript.mjs";
 import * as packs from "./utils/packs.mjs";
 
 export default gulp.series(
 	gulp.parallel(
 		css.compile,
-		// lang.compile,
+		lang.compile,
 		javascript.lint,
 		javascript.compile
 	),
 
 	gulp.parallel(
 		css.watchUpdates,
-		// lang.watchUpdates,
+		lang.watchUpdates,
 		javascript.watchUpdates
 	)
 );
 
 export const build = gulp.parallel(
 	css.compile,
-	// lang.compile,
+	lang.compile,
 	javascript.lint,
 	javascript.compile,
 	packs.compile
 );
 
-// export const clean = gulp.parallel(css.clean, lang.clean, packs.clean);
-export const clean = gulp.parallel(css.clean, packs.clean);
+export const clean = gulp.parallel(css.clean, lang.clean, packs.clean);
 export const compileCss = gulp.series(css.compile);
-// export const compileLang = gulp.series(lang.compile);
+export const compileLang = gulp.series(lang.compile);
 export const compilePacks = gulp.series(packs.compile);
 export const lintJs = gulp.series(javascript.lint);
