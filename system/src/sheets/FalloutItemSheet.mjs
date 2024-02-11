40418339f741be9732d391a6fc7c31ef22de5155
Paul Maskelyne <mu77ley@gmail.com>
2024-02-11 11:33:27 +0000

    revert auto height on item sheet

--- system/src/sheets/FalloutItemSheet.mjs
+++ system/src/sheets/FalloutItemSheet.mjs
@@ -13,7 +13,7 @@ export default class FalloutItemSheet extends ItemSheet {
 		return mergeObject(super.defaultOptions, {
 			classes: ["fallout", "sheet", "item"],
 			width: 520,
-			height: "auto",
+			height: 520,
 			tabs: [{
 				navSelector: ".sheet-tabs",
 				contentSelector: ".sheet-body",
